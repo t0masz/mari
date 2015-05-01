f61be97c78266f6329cced729d4ed3081a754027
TomasZ <tomas.zikmund@email.cz>
2015-05-01 15:50:55 +0200

    Scheduling calendar for priests

---
+++ app/presenters/PriestPresenter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	Nette\Forms\Controls\SubmitButton,
+	Model,
+	App\Forms;
+
+
+/**
+ * Priest presenter.
+ */
+class PriestPresenter extends BasePresenter
+{
+	/** @var Model\PriestManager @inject */
+	public $priestManager;
+
+
+	public function handlePriest($date, $time, $name = NULL, $id = NULL)
+	{
+		if($this->getUser()->isLoggedIn() && $this->getUser()->isAllowed('Priest','edit')) {
+			if(isset($name) && $name != '') {
+				if(isset($id) && $id > 0) {
+					$currentValue = $this->priestManager->getByID($id);
+					if($currentValue['names'] == $name) {
+						$values = array(
+							'id' => $id
+						);
+					} else {
+						$values = array(
+							'id' => $id,
+							'names' => $name
+						);
+					}
+				} else {
+					$values = array(
+						'date' => $date,
+						'time' => $time,
+						'names' => $name
+					);
+				}
+				if (isset($values['names']) && $values['names'] != '')
+					$result = $this->priestManager->save($values);
+				elseif (isset($id) && $id > 0)
+					$result = $this->priestManager->deleteById($id);
+				else
+					$this->flashMessage('Chyba při zpracování požadavku!', 'danger');
+			} else {
+				$this->flashMessage('Nemáš zadané jméno!', 'danger');
+			}
+		} else {
+			$this->flashMessage('Nemáš práva k editaci služeb!', 'danger');
+		}
+		$date = new \DateTime($date);
+		$days = $date->format('j')-1;
+		$date = $date->sub(new \DateInterval("P{$days}D"));
+		$this->template->items = $this->priestManager->findByDate($date->format('Y-m-d'));
+		$this->template->prev = $date->sub(new \DateInterval('P1M'))->format('Y-m-d');
+		$this->template->next = $date->add(new \DateInterval('P2M'))->format('Y-m-d');
+		$this->redrawControl();
+	}
+
+	public function renderDefault($date = NULL)
+	{
+		$date = new \DateTime($date);
+		$days = $date->format('j')-1;
+		$date = $date->sub(new \DateInterval("P{$days}D"));
+		$this->template->items = $this->priestManager->findByDate($date->format('Y-m-d'));
+		$this->template->prev = $date->sub(new \DateInterval('P1M'))->format('Y-m-d');
+		$this->template->next = $date->add(new \DateInterval('P2M'))->format('Y-m-d');
+		if($this->getUser()->isLoggedIn() && $this->getUser()->isAllowed('Priest','edit')) {
+			$this->template->text = "editace pouze pro přihlášené s právy";
+		} else {
+			$this->template->text = "viditelné pro ostatní, neindexovat vyhledávači";
+		}
+		if($this->isAjax()) {
+			$this->redrawControl();
+		}
+	}
+
+}
