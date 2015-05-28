aed37f97658c3ef6a14479fe1ef710e0a264623a
TomasZ <tomas.zikmund@email.cz>
2015-05-28 18:43:43 +0200

    Revert "Merge branch 'master' into photogallery"
    This reverts commit 1416a107fb2ed8b0283aa9106e9eb9d198931575, reversing
    changes made to 59172987276a79cb3982d9386e0abacc9208f6cd.

--- app/model/PriestManager.php
+++ app/model/PriestManager.php
@@ -56,6 +56,7 @@ class PriestManager extends Nette\Object
 	 */
 	public function findByDate($date)
 	{
+#		\Tracy\Debugger::barDump($date);
 		$from = new \DateTime($date);
 		$date = new \DateTime($date);
 		$month['days'] = $date->format('t');
@@ -69,11 +70,14 @@ class PriestManager extends Nette\Object
 		}
 		$to = $date->sub(new \DateInterval("P1D"));
 		$result = $this->priestRepository->findBySql('date BETWEEN ? AND ?',array($from->format('Y-m-d'),$to->format('Y-m-d')));
+#		\Tracy\Debugger::barDump($services);
 		foreach($result as $item) {
-			if ($item->time->format('%H:%I:%S') == '07:00:00') {
+#			\Tracy\Debugger::barDump($item->time->format('%H:%I:%S'));
+			if ($item->time->format('%H:%I:%S') == '07:30:00') {
+#				\Tracy\Debugger::barDump($item->time);
 				$services[$item->date->format('Y-m-d')]['names1'] = $item->names;
 				$services[$item->date->format('Y-m-d')]['id1'] = $item->id;
-			} elseif ($item->time->format('%H:%I:%S') == '08:30:00') {
+			} elseif ($item->time->format('%H:%I:%S') == '09:00:00') {
 				$services[$item->date->format('Y-m-d')]['names2'] = $item->names;
 				$services[$item->date->format('Y-m-d')]['id2'] = $item->id;
 			} elseif ($item->time->format('%H:%I:%S') == '18:00:00') {
@@ -99,10 +103,13 @@ class PriestManager extends Nette\Object
 	 */
 	public function save($values)
 	{
+		\Tracy\Debugger::log($values);
+#		return TRUE;
 		if (isset($values['id']) && ($values['id'] > 0)) {
 			$id = $values['id'];
 			unset($values['id']);
 			$result = $this->priestRepository->findBy(array('id' => (int)$id))->update($values);
+#			\Tracy\Debugger::log($result);
 			$return = $result > 0 ? 'updated' : FALSE;
 		} else {
 			$result = $this->priestRepository->insert($values);
